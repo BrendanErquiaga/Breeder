a27eb2af0dd8a9bf977bcb23df03ab85fe083a43
brendanerquiaga <brendanerquiaga@gmail.com>
2018-08-06 18:04:43 -0700

    Messing with ajax requests for chicken endpoint

--- BasicFrontEnd/SimpleChickenVisualizer/js/chickenVisualizer.js
+++ BasicFrontEnd/SimpleChickenVisualizer/js/chickenVisualizer.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var chickenBlock = '<div class="chickenContainer" id="customChicken1"><div class="chicken"><div class="wing"></div><div class="eye"></div><div class="beak_top"></div><div class="crest crest1"></div><div class="crest crest2"></div><div class="crest crest3"></div><div class="wattle"></div></div><p>ID: <span class="chickenId"></span><br>Name: <span class="chickenName"></span></p></div>"',
-  chickenCount = 0;
+  chickenCount = 0,
+  getChickenURL = "https://du1ejfbfoe.execute-api.us-west-2.amazonaws.com/DEV/organism/";
 
 $(document).ready(function() {
     pageLoad();
@@ -12,6 +13,21 @@ function pageLoad() {
   addCustomChicken(burntChickenDNA);
   addCustomChicken(leghornChickenDNA);
   addCustomChicken(plymouthChickenDNA);
+  callGetChickenURL(0);
+}
+
+function callGetChickenURL(chickenID) {
+  var fullUrl = getChickenURL + chickenID;
+
+  $.ajax({
+        url: getChickenURL,
+        type: 'GET',
+        dataType: 'application/json',
+        success: function(data){
+            console.log("You can do anything!");
+            console.log(JSON.stringify(data));
+        }
+  });
 }
 
 function addCustomChicken(chickenDNA) {
