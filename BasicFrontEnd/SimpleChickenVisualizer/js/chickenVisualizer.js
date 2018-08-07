d2b14bd17f9112c417b6393d375e5bb3c846ed41
brendanerquiaga <brendanerquiaga@gmail.com>
2018-08-07 12:06:19 -0700

    Loads chickens from API

--- BasicFrontEnd/SimpleChickenVisualizer/js/chickenVisualizer.js
+++ BasicFrontEnd/SimpleChickenVisualizer/js/chickenVisualizer.js
@@ -2,31 +2,34 @@
 
 var chickenBlock = '<div class="chickenContainer" id="customChicken1"><div class="chicken"><div class="wing"></div><div class="eye"></div><div class="beak_top"></div><div class="crest crest1"></div><div class="crest crest2"></div><div class="crest crest3"></div><div class="wattle"></div></div><p>ID: <span class="chickenId"></span><br>Name: <span class="chickenName"></span></p></div>"',
   chickenCount = 0,
-  getChickenURL = "https://du1ejfbfoe.execute-api.us-west-2.amazonaws.com/DEV/organism/";
+  getChickenURL = "https://du1ejfbfoe.execute-api.us-west-2.amazonaws.com/DEV/organism/",
+  chickenIDs = [ 0, 1, 2, 3, 4];
 
 $(document).ready(function() {
     pageLoad();
 });
 
 function pageLoad() {
-  addCustomChicken(tropicalChickenDNA);
-  addCustomChicken(burntChickenDNA);
-  addCustomChicken(leghornChickenDNA);
-  addCustomChicken(plymouthChickenDNA);
-  callGetChickenURL(0);
+  loadChickens();
+}
+
+function loadChickens() {
+  for (var i = 0; i < chickenIDs.length; i++) {
+    callGetChickenURL(i);
+  }
+
+  console.log("All chickens loaded!");
 }
 
 function callGetChickenURL(chickenID) {
   var fullUrl = getChickenURL + chickenID;
 
-  $.ajax({
-        url: getChickenURL,
-        type: 'GET',
-        dataType: 'application/json',
-        success: function(data){
-            console.log("You can do anything!");
-            console.log(JSON.stringify(data));
-        }
+  var request = $.get(fullUrl, function(data) {
+    addCustomChicken(data);
+  });
+
+  request.fail(function() {
+    console.error("Error fetching chicken data: " + chickenID);
   });
 }
 
